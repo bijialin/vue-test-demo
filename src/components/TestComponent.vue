adff9d4ef83216749404a9864e3f198a8b6d96a8
bijialin <1640992373@qq.com>
2018-09-17 17:39:31 +0800

    init

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p>test vue component</p>
+        <div class="words">{{testWord}}</div>
+    </div>
+</template>
+<script>
+export default {
+  name: "testWord",
+  props: {
+    testWord: {
+      type: String
+    }
+  }
+};
+</script>
+<style scoped>
+.words {
+  color: brown;
+  font-size: 20px;
+  padding-top: 50px;
+  border: 1px solid green;
+  width: 200px;
+  height: 60px;
+  margin: 0 auto;
+}
+</style>
+
+
+
---
+++ src/views/Test.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="test">
+		<p>{{msg}}</p>	
+		<TestComponent :testWord="testWord"></TestComponent>		
+	</div> 
+</template>
+<script>
+//@ is src
+import TestComponent from "@/components/TestComponent.vue";
+
+export default {
+  data() {
+    return {
+      msg: "test vue page!",
+      testWord: "this is test word from parent!"
+    };
+  },
+  name: "test",
+  components: {
+    TestComponent
+  }
+};
+</script>
